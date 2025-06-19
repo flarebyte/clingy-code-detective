7a37e70b2b883cd9aefd450f309e210ad20c8fb5
Olivier Huin <olih@users.noreply.github.com>
2025-06-19 06:57:30 +0100

    Removes unused function

--- internal/scanner/walk_test.go
+++ internal/scanner/walk_test.go
@@ -148,12 +148,3 @@ func TestWalkDirectories(t *testing.T) {
 
 	}
 }
-
-// mapsValues extracts the values from a map[string]string.
-func mapsValues(m map[string]string) []string {
-	values := make([]string, 0, len(m))
-	for _, v := range m {
-		values = append(values, v)
-	}
-	return values
-}
